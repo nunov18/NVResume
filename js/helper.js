a409947b2074bd9b71d520cd60cf8ac32cc39912
nunov18 <nunov18@gmail.com>
2017-04-05 11:26:59 +0100

    NVResume

--- js/helper.js
+++ js/helper.js
@@ -47,7 +47,7 @@ var HTMLschoolName = '<a href="%url%">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolMajor = '<p class="major"><em><br>Major: %data%</em></p>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="%url%">%data%';
